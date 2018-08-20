6cc9ddc64886316d6d8955f0aeb5e1b0487293de
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-19 19:48:37 -0500

    We add exit code in case all digits were nines; Insert 1 at the beginning of array

--- adding.rb
+++ adding.rb
@@ -3,6 +3,12 @@ p "Adding One!"
 def add_one array, num = array.length - 1
 	i = num
 
+	if i < 0
+		array.insert(0, 1)
+
+		return array
+	end	
+
 	if array[i] < 9
 		array[i] += 1
 		return array
@@ -19,4 +25,8 @@ end
 
 
 p add_one [2, 4, 5]
+
+p add_one [2, 4, 9]
+
+p add_one [9, 9, 9]
 
